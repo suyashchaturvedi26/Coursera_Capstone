0522a1fd9361d8c57be6eef7aea5eaf6a12cbfc8
suyashchaturvedi26 <suyashchaturvedi.nitie2021@gmail.com>
2020-10-19 20:15:04 +0530

    Committing first file using Git

---
+++ Capstone_Project.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is my first Capstone Project. I have taken this course on coursera and it provides a wonderful starting point for the beginners in data science domain
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
